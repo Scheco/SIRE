ce36c217fc7b6a7e733fc940ad88525207b0100b
AbielOvx <33072692+AbielOvx@users.noreply.github.com>
2017-12-10 23:49:21 -0600

    Se importaron las entidades en el archivo views

--- principal/views.py
+++ principal/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from models import Proveedores
+from models import Negocios
+from models import Horarios
+from models import Trabajadores
+from models import Camiones
+from models import Materiales
+from models import MaterialRecolectado
+from models import Clientes
+from models import Ventas
