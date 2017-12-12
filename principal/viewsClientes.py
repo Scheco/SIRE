f72a5bf64bf71e209ab62bbcd8477d70663fa59d
sesevillaga <33072539+sesevillaga@users.noreply.github.com>
2017-12-12 14:44:33 -0600

    InsertarClientes
    El metodo de insertar clientes funcionando al 100%

--- principal/viewsClientes.py
+++ principal/viewsClientes.py
@@ -8,16 +8,16 @@ from models import Materiales
 
 
 def insertarClientes(request):
-    if 'idCliente' in request.POST and 'idMaterial' in request.POST and 'nombre' in request.POST and 'apellidoP' in request.POST and 'apellidoM' in request.POST and 'domicilio' in request.POST and 'telefono' in request.POST:
-            idClientes = request.POST['idCliente']
+    if "idCliente" in request.POST and "idMaterial" in request.POST and "nombre" in request.POST and "apellidoP" in request.POST and "apellidoM" in request.POST and "domicilio" in request.POST and "telefono" in request.POST:
+            idCliente = request.POST['idCliente']
             idMaterial = request.POST['idMaterial']
             nombre = request.POST['nombre']
             apellidoP = request.POST['apellidoP']
             apellidoM = request.POST['apellidoM']
             domicilio = request.POST['domicilio']
             telefono = request.POST['telefono']
-            Cl = Clientes(idCliente=idClientes,idMaterial_id=idMaterial,nombre=nombre,apellidoP=apellidoP,apellidoM=apellidoM,domicilio=domicilio,telefono=telefono)
-            Cl= save()
+            Cl = Clientes(idCliente=idCliente,nombre=nombre,apellidoP=apellidoP,apellidoM=apellidoM,domicilio=domicilio,telefono=telefono,idMaterial_id=idMaterial)
+            Cl.save()
             return render(request,'menu.html')
     else:
         return render(request,'insertarClientes.html')
