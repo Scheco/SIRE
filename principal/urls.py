44ff4238ff0c76b6f01283fab750a066a4b2cc31
redten <33072643+redten@users.noreply.github.com>
2017-12-11 23:01:05 -0600

    url de actualizado

--- principal/urls.py
+++ principal/urls.py
@@ -1,16 +1,13 @@
 from django.conf.urls import url
 from views import *
-<<<<<<< HEAD
-from viewsClientes import *
-
-urlpatterns = [
-url(r'guardarClientes/$',insertarClientes),
-=======
 from viewsHorarios import *
+from viewsClientes import *
+from viewTrabajadores import *
 
 urlpatterns = [
+url(r'insertarTrabajador/$',insertarTrabajador), 
+url(r'guardarClientes/$',insertarClientes),
 url(r'insertarHorario/$',insertarHorario),
->>>>>>> 8693ca1c412637f3631a1a6b79895cf1caeaa0ca
 url(r'^$',menu)
 
 ]
