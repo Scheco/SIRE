b03373057f36f1b9143f4d3279b9b97911944f93
sesevillaga <33072539+sesevillaga@users.noreply.github.com>
2017-12-11 21:31:22 -0600

    InsertarCliente
    Creacion del la tabla de insertar clientes

--- principal/urls.py
+++ principal/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from views import *
+from viewsClientes import *
 
 urlpatterns = [
-
+url(r'guardarClientes/$',insertarClientes),
 url(r'^$',menu)
 
 ]
--- principal/viewsClientes.py
+++ principal/viewsClientes.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from models import Materiales
 from models import Clientes
+
+def insertarClientes(request):
+    if 'id' in request.POST and 'tipo' in request.POST and 'nombre' in request.POST and 'apellidoP' in request.POST and 'apellidoM' in request.POST and 'domicilio' in request.POST and 'telefono' in request.POST:
+        idClientes = request.POST['id']
+        tipoMaterial = request.POST['tipo']
+        nombre = request.POST['nombre']
+        apellidoPaterno = request.POST['apellidoP']
+        apellidoMaterno = request.POST['apellidoM']
+        domicilio = request.POST['domicilio']
+        tel = request.POST['telefono']
+        p = Clientes(idCliente=idClientes,idMaterial_id=tipoMaterial,nombre=nombre,apellidoP=apellidoPaterno,apellidoM=apellidoMaterno,domicilio=domicilio,telefono=tel)
+        p = save()
+        return render(request,'insertarClientes.html')
+    else:
+        return render(request,'insertarClientes.html')
