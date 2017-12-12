59b2edbb9385edfae04b6715a204ee0b9e12b3b8
Rooiilml <33072407+Rooiilml@users.noreply.github.com>
2017-12-12 16:50:00 -0600

    Negocios, menu, modelos
    el insertar de negocios es auto incrementable, se borro un campo de mas en el manu, se cambio en modelos.py los id de las entidades de Negocios, Trabajadores y Proveedores de Integer a AutoField

--- principal/viewsNegocios.py
+++ principal/viewsNegocios.py
@@ -4,18 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 from models import Proveedores
 from models import Negocios
 
 def insertarNegocio(request):
-    if "idNegocio" in request.POST and "idProveedor" in request.POST and "nombreNegocio" in request.POST and "domicilio" in request.POST and "tel" in request.POST:
-            idNegocio = request.POST['idNegocio']
+    if  "idProveedor" in request.POST and "nombreNegocio" in request.POST and "domicilio" in request.POST and "tel" in request.POST:
             idProveedor= request.POST['idProveedor']
             nombreNegocio= request.POST['nombreNegocio']
             domicilio= request.POST['domicilio']
             tel=request.POST['tel']
-            Ne=Negocios(idNegocio=idNegocio,nombre=nombreNegocio,domicilio=domicilio,telefono=tel,idProveedores_id=idProveedor)
+            Ne=Negocios(nombre=nombreNegocio,domicilio=domicilio,telefono=tel,idProveedores_id=idProveedor)
             Ne.save()
             return render(request,'insertarNegocio.html')
     else:
