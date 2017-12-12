e8aae4bc76de7163c05442b1cecee7d2f014eee5
sesevillaga <33072539+sesevillaga@users.noreply.github.com>
2017-12-11 23:40:26 -0600

    clientes
    checar si se sube un registro

--- principal/viewsClientes.py
+++ principal/viewsClientes.py
@@ -3,20 +3,21 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Materiales
 from models import Clientes
+from models import Materiales
+
 
 def insertarClientes(request):
-    if 'id' in request.POST and 'tipo' in request.POST and 'nombre' in request.POST and 'apellidoP' in request.POST and 'apellidoM' in request.POST and 'domicilio' in request.POST and 'telefono' in request.POST:
-        idClientes = request.POST['id']
-        tipoMaterial = request.POST['tipo']
-        nombre = request.POST['nombre']
-        apellidoPaterno = request.POST['apellidoP']
-        apellidoMaterno = request.POST['apellidoM']
-        domicilio = request.POST['domicilio']
-        tel = request.POST['telefono']
-        p = Clientes(idCliente=idClientes,idMaterial_id=tipoMaterial,nombre=nombre,apellidoP=apellidoPaterno,apellidoM=apellidoMaterno,domicilio=domicilio,telefono=tel)
-        p = save()
-        return render(request,'insertarClientes.html')
+    if 'idCliente' in request.POST and 'idMaterial' in request.POST and 'nombre' in request.POST and 'apellidoP' in request.POST and 'apellidoM' in request.POST and 'domicilio' in request.POST and 'telefono' in request.POST:
+            idClientes = request.POST['idCliente']
+            idMaterial = request.POST['idMaterial']
+            nombre = request.POST['nombre']
+            apellidoP = request.POST['apellidoP']
+            apellidoM = request.POST['apellidoM']
+            domicilio = request.POST['domicilio']
+            telefono = request.POST['telefono']
+            Cl = Clientes(idCliente=idClientes,idMaterial_id=idMaterial,nombre=nombre,apellidoP=apellidoP,apellidoM=apellidoM,domicilio=domicilio,telefono=telefono)
+            Cl= save()
+            return render(request,'menu.html')
     else:
         return render(request,'insertarClientes.html')
