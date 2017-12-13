e13adab7dcd714883cff3fed6c982e724d21bd18
Jose Jesus Leyva <31327769+naniris@users.noreply.github.com>
2017-12-13 00:44:47 -0600

    Ya modifica y elimina a los proveedores

--- principal/viewsProveedores.py
+++ principal/viewsProveedores.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 
 from models import Proveedores
 
-# Metodo para insertar Proveedores
+# Metodo para registrar Proveedores
 def insertarProveedores(request):
     if  'nombreProveedor' in request.POST and 'apellidoP' in request.POST and 'apellidoM' in request.POST and 'edad' in request.POST and 'telefono' in request.POST and 'domicilio' in request.POST:
             nombreProveedor = request.POST['nombreProveedor']
@@ -22,6 +22,37 @@ def insertarProveedores(request):
     else:
         return render(request, 'insertarProveedores.html')
 
+# Metodo para consultar todos los datos de los proveedores ya registrados
 def consultarProveedores(request):
     registroProveedores = Proveedores.objects.all()
-    return render(request, 'consultarProveedores.html', {'registros':registroProveedores})
+    return render(request, 'consultarProveedores.html', {'registroProveedores':registroProveedores})
+
+# Metodo para mostrar todos los datos de los proveedores para modificar
+def modificarProveedores(request):
+    registro = Proveedores.objects.get(idProveedores=request.POST['idProveedores'])
+    return render (request, 'modificarProveedores.html', {'reg':registro})
+
+# Metodo para guardar uno o mas cambios en la vista de modificarProveedores.html
+def guardarProveedores(request):
+    p = Proveedores(idProveedores = request.POST['idProveedores'])
+    p.nombre = request.POST['nombreProveedor']
+    p.apellidoP = request.POST['apellidoP']
+    p.apellidoM = request.POST['apellidoM']
+    p.edad = request.POST['edad']
+    p.telefono = request.POST['telefono']
+    p.domicilio = request.POST['domicilio']
+    p.save()
+    registroProveedores = Proveedores.objects.all()
+    return render(request, 'consultarProveedores.html', {'msg': 'Se ha actualizado el registro correctamente','registroProveedores':registroProveedores})
+
+# Metodo para mostrar el id del proveedor que se va eliminar
+def feliminarProveedor(request):
+    idProveedores = request.GET['idProveedores']
+    return render(request, 'eliminarProveedores.html', {'idProveedores':idProveedores})
+
+# Metodo para eliminar el id del proveedor
+def eliminarProveedor(request):
+    p = Proveedores(idProveedores = request.POST['idProveedores'])
+    p.delete()
+    registroProveedores = Proveedores.objects.all()
+    return render(request, 'consultarProveedores.html', {'msg': 'Se ha eliminado el registro correctamente', 'registroProveedores':registroProveedores})
