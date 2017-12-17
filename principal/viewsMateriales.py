6369384cc66c64ab7b9dfe394d8064fc514f0690
charskill <holamundo95>
2017-12-16 20:44:08 -0600

    Se creo elmetodo para cargar la vista html insertarTrabajadores

---
+++ principal/viewsMateriales.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from models import Materiales
+
+def materiales(request):
+    return render(request,'insertarMateriales.html')
