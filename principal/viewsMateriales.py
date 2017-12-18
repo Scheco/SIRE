0dac76333965bc2f40f6ff36065290b761122f04
Rooiilml <33072407+Rooiilml@users.noreply.github.com>
2017-12-18 02:26:57 -0600

    viewsMateriales.py completo

--- principal/viewsMateriales.py
+++ principal/viewsMateriales.py
@@ -8,7 +8,39 @@ from models import Materiales
 
 def materiales(request):
     return render(request,'insertarMateriales.html')
-    
+
+def insertarMateriales(request):
+    if 'tipoMaterial' in request.POST and 'descripcionMaterial' in request.POST:
+            tipoMaterial = request.POST['tipoMaterial']
+            descripcionMaterial = request.POST["descripcionMaterial"]
+            m = Materiales(tipo=tipoMaterial, descripcion=descripcionMaterial)
+            m.save()
+            return render(request, 'insertarMateriales.html', {'msg': 'Se registro correctamente el material', 'm':m})
+    else:
+        return render(request, 'insertarMateriales.html')
 
 def consultarMateriales(request):
-    return render(request,'consultarMateriales.html')
++    registros = Materiales.objects.all()
+    return render (request,'consultarMateriales.html',{'registros':registros})
+
+def modificarMateriales(request):
+    m = Materiales(idMaterial=request.POST['id'])
+    m.tipo = request.POST['tipo']
+    m.descripcion = request.POST['descripcion']
+    m.save()
+    registros = Materiales.objects.all()
+    return render(request,'consultarMateriales.html',{'msg':'Se han guardado los cambios correctamente','registros':registros})
+
+def modificarM(request):
+    registro = Materiales.objects.get(idMaterial=request.POST['id'])
+    return render(request,'modificarMateriales.html',{'reg':registro})
+
+def eliminarM(request):
+    id = request.GET['id']
+    return render(request,'eliminarMateriales.html',{'id':id})
+
+def eliminarMateriales(request):
+    m = Materiales(idMaterial=request.POST['id'])
+    m.delete()
+    registros = Materiales.objects.all()
+    return render(request,'consultarMateriales.html',{'msg':'Se ha eliminado correctamente','registros':registros})
