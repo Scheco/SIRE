c003d9829667de1315bf39e247f4429a171299f6
Rooiilml <33072407+Rooiilml@users.noreply.github.com>
2017-12-18 03:11:32 -0600

    urls
    Se acomodaron las urls, ya estan todas las urls completas de las entidades de "Horarios", "Clientes", "Negocios"
    
    hay varias urls con problemas, estan indicadas con comentarios

--- principal/urls.py
+++ principal/urls.py
@@ -7,31 +7,55 @@ from viewsProveedores import *
 from viewsNegocios import *
 
 urlpatterns = [
-url(r'eliminarCamiones/$',eliminarCamion),
-url(r'modificarCamiones/$',modificarCamion),
-url(r'consultarCamion/$',consultarCamion),
-url(r'insertarCamion/$',insertarCamion),
-url(r'consltarTrabajadores/$',consltarTrabajadores),
-url(r'trabajadores/$',trabajadores),
-url(r'insertarNegocios/$',insertarNegocios),
+# ----------------URLS proveedores------------------
 url(r'guardarProveedores/$',insertarProveedores),
 url(r'proveedores/$',insertarProveedores),
+
+
+# --------------URLS camino----------------------
+# --------------hay problemas con estas URLS------------
+# url(r'eliminarCamiones/$',eliminarCamion),
+# url(r'modificarCamiones/$',modificarCamion),
+# url(r'consultarCamion/$',consultarCamion),
+# url(r'insertarCamion/$',insertarCamion),
+
+
+# -------------URLS proveedores-----------------
+# url(r'consltarTrabajadores/$',consltarTrabajadores),      (hay problemas con esta url)
+url(r'trabajadores/$',trabajadores),
 url(r'insertarTrabajador/$',insertarTrabajador),
-url(r'guardarClientes/$',insertarClientes),
+
+
+# -----------URLS horarios------------------
 url(r'insertarHorario/$',insertarHorario),
 url(r'consultarHorarios/$',consultarHorarios),
 url(r'modificarHorarios/$',modificarHorarios),
 url(r'guardarHorarios/$',guardarHorarios),
 url(r'horariosEliminar/$',horariosEliminar),
 url(r'eliminarHorarios/$',eliminarHorarios),
+
+# -------------URLS clientes--------------------
 url(r'eliminarCli/$',eliminarCli),
 url(r'eliminarCliente/$',eliminarCliente),
 url(r'guardarCambiosClientes/$',guardarCambiosClientes),
 url(r'modificarClientes/$',modificarClientes),
-
 url(r'consultarClientes/$',consultarClientes),
 url(r'guardarClientes/$',insertarClientes),
 url(r'clientes/$',insertarClientes),
+
+# ---------------URLS negocios----------------------
+url(r'eliminarNegocios/$',eliminarNegocios),
+url(r'eliminarNeg/$',eliminarNeg),
+url(r'guardarNegocios/$',guardarNegocios),
+url(r'consultarNegocios/modificarNegocio/$',modificarNegocios),
+url(r'consultarNegocios/$',consultarNegocios),
+url(r'insertarNegocios/$',insertarNegocios),
+url(r'negocios/$',insertarNegocios),
+
+
+
+
+
 url(r'^$',menu),
 
 ]
