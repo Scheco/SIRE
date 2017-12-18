2af4ade4188d8e3f0ea7a8aa211954d50ce96d0d
Chayras34 <spider_kio_56@hotmail.com>
2017-12-18 08:24:12 -0600

    URL Material Recolectado
    se agregaron url's

--- principal/urls.py
+++ principal/urls.py
@@ -6,6 +6,7 @@ from viewTrabajadores import *
 from viewsProveedores import *
 from viewsNegocios import *
 from viewsCamiones import *
+from viewsMaterialRecolectado import *
 
 urlpatterns = [
 # ----------------URLS proveedores------------------
@@ -52,6 +53,13 @@ url(r'consultarNegocios/modificarNegocio/$',modificarNegocios),
 url(r'consultarNegocios/$',consultarNegocios),
 url(r'insertarNegocios/$',insertarNegocios),
 url(r'negocios/$',insertarNegocios),
+
+#----------------URLS---------------------------
+url(r'modificarMaterialRecolectado/$',consultarMaterialesRecolectados),
+url(r'consultarMaterialesRecolectados/$',consultarMaterialesRecolectados),
+url(r'insertarMaterialesRecolectados/$',insertarMaterialesRecolectados),
+url(r'materialRecolectado/$',insertarMaterialesRecolectados),
+
 
 
 
